5e14f2d847391bc57201e5f9399a78bbb9fedf91
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-09-11 10:30:37 -0700

    add more file structure ready for testing

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+// must be in loswer calem case
+var findandReplace
+
+
+// debuddeg turn on/off
+//debugger;
+
+
+
+
+// this part stays commented out for now but needat the end to actually display working code!  
+
+// $(document).ready(function()  {
+//
+//     $("form#countupby").submit(function(event) {
+//         var endNumber = ($("input#endNumber").val());
+//         var incrementer = ($("input#incrementer").val());
+//         var result = countUpBy(endNumber, incrementer);
+//
+//         $(".output").text(result);
+//
+//         $("#result").show();
+//         event.preventDefault();
+//     });
+// });
