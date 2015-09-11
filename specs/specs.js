f4f2022c3e9b2fd3e690a34ab5aaa3eed264a3b4
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-09-11 10:43:19 -0700

    first test

--- specs/specs.js
+++ specs/specs.js
@@ -1,1 +1,20 @@
-testing done here! 
+// test 1
+describe("wordReplacer", function() {
+  it("returns the word that was inputed by the user", function() {
+    expect(wordReplace("taco", "taco", "burrito")).to.equal("burrito");
+  });
+
+// test 2
+
+// test 3
+
+// test 4
+
+
+
+
+
+
+
+//stays at bottom
+  });
